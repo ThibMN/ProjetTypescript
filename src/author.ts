5b91f5843d8a28fe095f2d209fbd66b2485d9728
Eloane Ducrocq <elo.ane.ducrocq@gmail.com>
2025-11-14 14:43:31 +0100

    ajout du fichier book

--- src/author.ts
+++ src/author.ts
@@ -10,4 +10,4 @@ export const demoAuthors: Author[] = [
   { id: 1, name: "Victor Hugo", birthYear: 1802 },
   { id: 2, name: "Simone de Beauvoir", birthYear: 1908 },
   { id: 3, name: "Albert Camus" }
-];
+]; 
