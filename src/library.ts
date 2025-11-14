f25bde1e9492a6cec4e3ccf3c381ac5c5c17be6d
Rafus455 <kwasnikrayan45@gmail.com>
2025-11-14 14:49:27 +0100

    premier commit de rayan

--- src/library.ts
+++ src/library.ts
@@ -5,12 +5,25 @@ import { Loan, LoanStatus, demoLoans, hasReachedLoanLimit } from "./loan";
 import { Student } from "./user";
 
 export class Library {
-    private books: Book[] = [];
-    private loans: Loan[] = [];
+  private books: Book[] = [];
+  private loans: Loan[] = [];
 
-    constructor(initialBooks: Book[] = [], initialLoans: Loan[] = []) {
-        this.books = [...initialBooks];
-        this.loans = [...initialLoans];
+  constructor(initialBooks: Book[] = [], initialLoans: Loan[] = []) {
+    this.books = [...initialBooks];
+    this.loans = [...initialLoans];
+  }
+
+  addBook(book: Book): void {
+    if (this.findBookById(book.id)) {
+      throw new Error(`Le livre ${book.id} existe deja.`);
     }
+    this.books.push(book);
+  }
 
-}
+  listAvailable(): Book[] {
+    return this.books.filter((book) => book.available);
+  }
+
+  findBookById(id: number): Book | undefined {
+    return this.books.find((book) => book.id === id);
+  }
